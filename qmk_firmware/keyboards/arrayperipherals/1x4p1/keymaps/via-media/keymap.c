01969efe7b6f4e318648edfd787709298d53f05d
starkayc <5252904+starkayc@users.noreply.github.com>
2021-05-26 10:04:07 -0400

    personal config for qmk and hex files.

---
+++ qmk_firmware/keyboards/arrayperipherals/1x4p1/keymaps/via-media/keymap.c
@@ -0,0 +1,18 @@
+#include QMK_KEYBOARD_H
+
+void encoder_update_user(uint8_t index, bool clockwise) { // Encoder
+  if (index == 0) {
+    if (clockwise) {
+      tap_code(KC_VOLU); // Volume Up
+    } else {
+      tap_code(KC_VOLD); // Volume Down
+    }
+  }
+}
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[0] = LAYOUT_ortho_1x5(KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, TG(1)), // Layer 0
+	[1] = LAYOUT_ortho_1x5(KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, TG(1)), // Layer 1
+  [2] = LAYOUT_ortho_1x5(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO), // Layer 2
+  [3] = LAYOUT_ortho_1x5(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO) // Layer 3
+};
