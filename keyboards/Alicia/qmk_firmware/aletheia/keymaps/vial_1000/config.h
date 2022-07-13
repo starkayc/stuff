0d351a58e4e51b4b6a33fcf600ba6f7ba155d4e9
starkayc <5252904+starkayc@users.noreply.github.com>
2022-07-12 23:23:36 -0400

    add alice

---
+++ keyboards/Alicia/qmk_firmware/aletheia/keymaps/vial_1000/config.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3B, 0x53, 0x48, 0x86, 0x2F, 0x0C, 0x93, 0xC5}
+
+/* Change USB Polling Rate to 1000hz and a larger keys per scan for elite gaming*/
+#define USB_POLLING_INTERVAL_MS 1
+#define QMK_KEYS_PER_SCAN 12
